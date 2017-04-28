0f49be4e89bec48d476620946ce4d0d6c358bf61
leminhtr <leminhtr@etu.utc.fr>
2017-04-28 10:30:48 +0200

    query bio + question_id

--- es_query.js
+++ es_query.js
@@ -119,3 +119,160 @@ var query_UL_phy_avg={
         }
     }
 };
+
+
+var query_UL_phy_avg_quest_id={
+    "from":0, "size":1000,
+    "query": {
+        "bool": {
+            "must":
+                [
+                    {"match":{ "type":"XContentApi"}},
+                    {"match":{"depot_path": "/Partenaires/UL/UL-Phy01"}},
+                    {"exists": {"field": "score_scaled"}}
+                ],
+
+            "must_not":
+                [
+                    {"match": {"question_id":"qgMx9nWg3feUknCfwecgli"}},
+                    {"match": {"question_id":"pky45GVJlvk0YYYe0wiTxe"}},
+                    {"match": {"question_id":"KDiQi0UNikeZBzySvmA2k"}},
+                    {"match": {"question_id":"Dvj5AqVq4RiIbIFWaJuSud"}},
+                    {"match": {"question_id":"qVTVybVQlxckqCv1EI54h"}},
+                    {"match": {"question_id":"udFJDEATISe7oQfqnd9Kki"}}
+                ]
+        }
+    },
+    "aggregations": {
+        "avg_quest": {
+            "terms": {
+                "field": "question_id",
+                "size":100
+            },
+            "aggregations": {
+                "score_avg": {
+                    "avg": {
+                        "field": "score_scaled"
+                    }
+                }
+            }
+        }
+    }
+};
+
+// ******************************* UL-BIO ************************************************
+
+
+var query_UL_Bio_avg_user_id={
+    "from":0, "size":1000,
+    "query": {
+        "bool": {
+            "must":
+                [
+                    {"match":{ "type":"XContentApi"}},
+                    {"match":{"depot_path": "/Partenaires/UL/UL-Bio"}},
+                    {"exists": {"field": "score_scaled"}}
+                ],
+
+            "must_not":
+                [
+                    {"match": {"question_id":"i6nRY2FhjtlKF4kAuRVlLi"}},
+                    {"match": {"question_id":"j74GTDxCdUh1AhUJbxzOCg"}},
+                    {"match": {"question_id":"Ro8Bn94sH5fiObRDLGMDub"}},
+                    {"match": {"question_id":"AXGE9teKhMdo4j0BsK6x8f"}},
+                    {"match": {"question_id":"AXGE9teKhMdo4j0BsK6x8f"}},
+                    {"match": {"question_id":"XSSBiN8sCL10gnc6XsIUi"}},
+                    {"match": {"question_id":"ImUQEzT8sjgXe0C5WIYD9g"}},
+                    {"match": {"question_id":"QWGy9VVCetknLGk0eUfkvc"}}
+                ]
+        }
+    },
+    "aggregations": {
+        "avg_quest": {
+            "terms": {
+                "field": "user.raw",
+                "size":100
+            },
+            "aggregations": {
+                "score_avg": {
+                    "avg": {
+                        "field": "score_scaled"
+                    }
+                }
+            }
+        }
+    }
+};
+
+
+
+var query_UL_Bio_avg_quest_id={
+    "from":0, "size":1000,
+    "query": {
+        "bool": {
+            "must":
+                [
+                    {"match":{ "type":"XContentApi"}},
+                    {"match":{"depot_path": "/Partenaires/UL/UL-Bio"}},
+                    {"exists": {"field": "score_scaled"}}
+                ],
+
+            "must_not":
+                [
+                    {"match": {"question_id":"i6nRY2FhjtlKF4kAuRVlLi"}},
+                    {"match": {"question_id":"j74GTDxCdUh1AhUJbxzOCg"}},
+                    {"match": {"question_id":"Ro8Bn94sH5fiObRDLGMDub"}},
+                    {"match": {"question_id":"AXGE9teKhMdo4j0BsK6x8f"}},
+                    {"match": {"question_id":"AXGE9teKhMdo4j0BsK6x8f"}},
+                    {"match": {"question_id":"XSSBiN8sCL10gnc6XsIUi"}},
+                    {"match": {"question_id":"ImUQEzT8sjgXe0C5WIYD9g"}},
+                    {"match": {"question_id":"QWGy9VVCetknLGk0eUfkvc"}}
+                ]
+        }
+    },
+    "aggregations": {
+        "avg_quest": {
+            "terms": {
+                "field": "question_id",
+                "size":100
+            },
+            "aggregations": {
+                "score_avg": {
+                    "avg": {
+                        "field": "score_scaled"
+                    }
+                }
+            }
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
